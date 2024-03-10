c38ec2febe02401690cb866b2f11b411fed19b15
OksanaBakhaieva <obahaeva@gmail.com>
2024-03-10 10:45:00 +0000

    changes

--- js/07-pokemons.js
+++ js/07-pokemons.js
@@ -2,17 +2,48 @@
  * Використовуємо https://pokeapi.co/ та створимо сторінку перегляду покемонів
  */
 
-function fetchPokemon(pokemonID) {
+function fetchPokemon(pokemonId) {
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId.toLowerCase()}`).then(
+    (resp) => {
+      if (!resp.ok) {
+        throw new Error(resp.statusText);
+      }
 
+      return resp.json();
+    }
+  );
 }
 
 const cardContainer = document.querySelector(".card-container");
-const searchForm = document.querySelector("#searchForm");
+const searchForm = document.querySelector(".search-form");
 
+searchForm.addEventListener("submit", handleSearch);
 
-function renderPokemonCard(pokemonID) {
-  
+function handleSearch(event) {
+  event.preventDefault();
+
+  const form = event.currentTarget;
+  const query = form.elements.query.value;
+
+  console.log(query);
+
+  fetchPokemon(query)
+    .then(renderPokemonCard)
+    .catch(onFetchError)
+    .finally(() => form.reset());
+
+  //.then(data => renderPokemonCard(data))
 }
+
+function renderPokemonCard({ name, sprites, weight, height, abilities }) {
+  const abilityListItems = abilities
+    .map(
+      (ability) => `<li class="list-group-item">${ability.ability.name}</li>`
+    )
+    .join("");
+  console.log(abilityListItems);
+
+
 const markup = `<div class="card">
    <div class="card-img-top">
      <img src="${sprites.front_default}" alt="${name}">
@@ -26,3 +57,10 @@ const markup = `<div class="card">
      <ul class="list-group">${abilityListItems}</ul>
    </div>
 </div>`;
+  cardContainer.innerHTML = markup;
+}
+
+function onFetchError(error) {
+  alert("Упс, щось пішло не так і ми не знайшли вашого покемона!");
+  console.error(error);
+}
--- js/08-last lesson.js
+++ js/08-last lesson.js
@@ -1,1 +1,63 @@
-// http://newsapi.org/
+console.log(true && 3); // 3
+console.log(false && 3); // false
+console.log(true && 4 && "kiwi"); //kiwi
+console.log(true && 0 && "kiwi"); // 0
+console.log(true || 3); //true
+console.log(true || 3 || 4); // true
+console.log(true || false || 7); // true
+console.log(1 && null && 2); // null
+console.log((1 && null && 2)> 0) ; // false
+console.log(null || (2 && 3) || 4); // 3
+
+//===============================================================
+
+function logItems(arr) {
+       for(let i = 0; i < arr.length; i++) {
+        console.log(`${i + 1} - ${arr[i]}`);
+          }
+}
+logItems(["Mango", "Polly"])
+
+//================================================================
+
+function printInfo(names, phones) {
+    const arrNames = names.split(",")
+    const arrPhones = phones.split(",")
+    console.log(arrNames);
+    console.log(arrPhones);
+    for (let i = 0; i < arrNames.length; i++){
+         console.log(`Name: ${arrNames[i]}, phone number: ${arrPhones[i]}`)
+        }
+    }
+printInfo("Jacob,William,Artemis", "07748300640,07748300641,07748300642")  
+
+//=================================================================
+let min = 0;
+    let hours = 0;
+    
+function formatTime(minutes) {
+    hours = String(Math.floor(minutes / 60)).padStart(2, 0);
+    min = String(minutes % 60).padStart(2, 0);
+    return `${hours}:${min}`;
+}
+    
+
+console.log(formatTime(70))
+console.log(formatTime(450))
+console.log(formatTime(1441))
+
+//=================================================================
+
+const data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+function getCombination(arr, num) {
+    const res = [];
+    for (let i = 0; i < arr.length; i += num){
+        console.log(i);
+        const comb = arr.slice(i, i + num)
+        res.push(comb)
+    }
+    return res;
+}
+
+console.log(getCombination(data, 3))
